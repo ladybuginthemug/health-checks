7260442fd7a70ab21485599988b3f53b92021f8d
xenya <88084724+ladybuginthemug@users.noreply.github.com>
2023-10-26 00:49:34 +0200

    update inpath.sh

--- inpath.sh
+++ inpath.sh
@@ -2,8 +2,11 @@
 
 in_path()
 {
-   cmd=$1        ourpath=$2         result=1
-   oldIFS=$IFS   IFS=":"
+   cmd=$1        
+   ourpath=$2
+   result=1
+   oldIFS=$IFS   
+   IFS=":"
 
    for directory in “$ourpath”
    do
